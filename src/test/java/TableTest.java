1b340e82550eda66ae344aa144fac1a75921f4bd
aliceyang <aliceyang@berkeley.edu>
2018-04-26 20:06:52 -0700

    ninth test

--- src/test/java/TableTest.java
+++ src/test/java/TableTest.java
@@ -221,6 +221,38 @@ public class TableTest {
         return stduents;
     }
 
+    @Test
+    public void testGetFrom() {
+        List<String> columnTitles = new ArrayList<>();
+        columnTitles.add("ID");
+        columnTitles.add("Name");
+        columnTitles.add("Major");
+        Table table = new Table(columnTitles);
+        String[] values = new String[]{"1", "Alice", "MIMS"};
+        table.add(values);
+        String[] values1 = new String[]{"2", "Jason", "EECS"};
+        table.add(values1);
+
+        List<String> columnTitles1 = new ArrayList<>();
+        columnTitles1.add("SID");
+        columnTitles1.add("Name");
+        columnTitles1.add("Nationality");
+        Table table1 = new Table(columnTitles);
+        String[] values3 = new String[]{"2", "Jason", "USA"};
+        table1.add(values3);
+
+        String name = "ID";
+        Column c = new Column(name, table);
+        String a = c.getFrom(0);
+        String b = "1";
+        assertEquals(b, a);
+
+        String name1 = "Name";
+        Column c1 = new Column(name1, table, table1);
+        String d = c1.getFrom(1, 0);
+        String e = "Jason";
+        assertEquals(e, d);
+    }
 
 
 
